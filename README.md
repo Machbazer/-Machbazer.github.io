5001efb6502536964d2fa0fd3406179bd5a5c306
Machbazer <167606100+Machbazer@users.noreply.github.com>
2024-04-20 13:15:41 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# -Machbazer.github.io
+# MachBazaar is the complete UI of an e-commerce website. The owner of the website is selling fish across the whole Dhaka city using it. I build this UI using HTML, CSS, Bootstrap, Javascript, and jquery.
+
+
+@2024
---
+++ js/script.js
@@ -0,0 +1,86 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      769:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 10) nav.className = 'navbar-bg-color navbar navbar-light pl-lg-0 py-lg-0 border-bottom border-white navbar-expand-lg'; 
+  else nav.className = 'scroll navbar navbar-light pl-lg-0 py-lg-0 border-bottom border-white navbar-expand-lg';
+};
+
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("a");
+console.log(tabTogglers);
+
+
+tabTogglers.forEach(function(toggler) {
+    toggler.addEventListener("click", function(e) {
+      e.preventDefault();
+  
+      let tabName = this.getAttribute("href");
+  
+      let tabContents = document.querySelector("#tab-contents");
+  
+      for (let i = 0; i < tabContents.children.length; i++) {
+        
+        tabTogglers[i].classList.remove("font-weight-bold", "border-bottom", "border-dark");  
+        tabContents.children[i].classList.remove("d-none");
+        if ("#" + tabContents.children[i].id === tabName) {
+          continue;
+        }
+        tabContents.children[i].classList.add("d-none");
+        
+      }
+      e.target.classList.add("font-weight-bold", "border-bottom", "border-dark");
+    });
+  });
+  
+document.getElementById("default-tab").click();
+
+
+// javascript for mobile tabs
+
+let mobileTabsContainer = document.querySelector("#mobile-tabs");
+
+let mobileTabTogglers = mobileTabsContainer.querySelectorAll("a");
+console.log(mobileTabTogglers);
+
+
+mobileTabTogglers.forEach(function(toggler) {
+    toggler.addEventListener("click", function(e) {
+      e.preventDefault();
+  
+      let tabName = this.getAttribute("href");
+  
+      let mobileTabContents = document.querySelector("#mobile-tab-contents");
+  
+      for (let i = 0; i < mobileTabContents.children.length; i++) {
+        
+        mobileTabTogglers[i].classList.remove("font-weight-bold", "border-bottom", "border-dark");  
+        mobileTabContents.children[i].classList.remove("d-none");
+        if ("#" + mobileTabContents.children[i].id === tabName) {
+          continue;
+        }
+        mobileTabContents.children[i].classList.add("d-none");
+        
+      }
+      e.target.classList.add("font-weight-bold", "border-bottom", "border-dark");
+    });
+  });
+  
+document.getElementById("mobile-default-tab").click();
+
